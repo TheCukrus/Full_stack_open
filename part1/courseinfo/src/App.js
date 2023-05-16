f71e5ae3a05af6746381390c01493100e895c95b
Jevgenij <sssgustavsss@gmail.com>
2023-05-16 17:18:47 +0300

    1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,8 +23,8 @@ const App = () =>
     <div>
 
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3}  />
-      {/* <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /> */}
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
