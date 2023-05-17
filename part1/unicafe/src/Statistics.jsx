88e9c410ecaa6c46e82f67f963ec19d86f89451a
Jevgenij <sssgustavsss@gmail.com>
2023-05-18 00:08:37 +0300

    1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -10,13 +10,18 @@ const Statistics = (props) =>
 
     return (
         <div>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive}%</p>
+            {all === 0 ?
+                <p>No feedback given</p> :
+                <div>
+                    <h1>statistics</h1>
+                    <p>good {good}</p>
+                    <p>neutral {neutral}</p>
+                    <p>bad {bad}</p>
+                    <p>all {all}</p>
+                    <p>average {average}</p>
+                    <p>positive {positive}%</p>
+                </div>
+            }
         </div>
     )
 }
