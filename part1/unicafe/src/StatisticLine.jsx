310076a4170a823a33c49791de50850f2d518c38
Jevgenij <sssgustavsss@gmail.com>
2023-05-18 07:46:26 +0300

    1.11*

--- part1/unicafe/src/StatisticLine.jsx
+++ part1/unicafe/src/StatisticLine.jsx
@@ -4,10 +4,12 @@ const StatisticsLine = (props) =>
 {
     const { text, value } = props;
     return (
+        <tbody>
         <tr>
             <td>{text}</td>
             <td>{value}</td>
         </tr>
+        </tbody>
     )
 }
 
