8e949f1a6671fb3bfe5632a6a87ca943d76cb11e
Jevgenij <sssgustavsss@gmail.com>
2023-05-21 00:14:25 +0300

    2.9*

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,11 +3,17 @@ import { useState } from 'react'
 const App = () =>
 {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
+  //submit
   const handleSubmit = (e) =>
   {
     e.preventDefault();
@@ -15,8 +21,7 @@ const App = () =>
 
     if (check !== undefined)
     {
-      alert(`${newName} is already added to phonebook`)
-      return;
+      return alert(`${newName} is already added to phonebook`)
     }
 
     const copyPhonebook = [...persons];
@@ -26,16 +31,29 @@ const App = () =>
     setNewNumber("");
   }
 
+  //onchange
   const handleNameOnChange = (e) => setNewName(e.target.value)
   const handleNumberOnChange = (e) => setNewNumber(e.target.value)
+  const handleSearch = (e) => setSearch(e.target.value);
 
+  let shownContacts = persons;
 
-
-
+  if (search !== "")
+  {
+    shownContacts = shownContacts.filter((contact) =>
+      contact.name.toLowerCase().includes(search.toLowerCase()))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        <p>filer shown with <input type="search" onChange={handleSearch} /></p>
+      </div>
+
+      <h2>add a new</h2>
+
       <form onSubmit={handleSubmit}>
         <div>
           name: <input type="text" onChange={handleNameOnChange} value={newName} />
@@ -48,7 +66,8 @@ const App = () =>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((ele) => <p key={ele.name}>{ele.name} {ele.number}</p>)}
+
+      {shownContacts.map((ele) => <p key={ele.id}>{ele.name} {ele.number}</p>)}
     </div>
   )
 }
