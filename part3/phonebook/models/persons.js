c42cbe9e9fec1c18edd7590235cb34d5399ba38f
Jevgenij <sssgustavsss@gmail.com>
2023-06-04 13:47:53 +0300

    3.20*

--- part3/phonebook/models/persons.js
+++ part3/phonebook/models/persons.js
@@ -25,7 +25,17 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
     "name": { type: String, minLength: 3 },
-    "number": String
+    "number": {
+        type: String,
+        validate: {
+            validator: function (v)
+            {
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        require: [true, `User phone number required`]
+    }
 })
 
 personSchema.set('toJSON', {
