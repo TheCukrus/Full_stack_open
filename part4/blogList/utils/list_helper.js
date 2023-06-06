03d1304b040d555c5c761c93a9f4b57c41859d04
Jevgenij <sssgustavsss@gmail.com>
2023-06-06 18:34:59 +0300

    4.4

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -4,4 +4,10 @@ const dummy = (blogs) =>
     return 1;
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) =>
+{
+    const likeCounter = blogs.reduce((total, currentValue) => total + currentValue.likes, 0)
+    return likeCounter;
+}
+
+module.exports = { dummy, totalLikes }
