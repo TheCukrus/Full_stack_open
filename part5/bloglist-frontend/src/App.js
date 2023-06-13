c85804ef2a1a86b488f1c87adbbdd69e90569b91
Jevgenij <sssgustavsss@gmail.com>
2023-06-13 10:27:47 +0300

    5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -38,6 +38,8 @@ const App = () =>
         :
         <div>
           <h2>blogs</h2>
+
+          <p>{user.username} logged in</p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
