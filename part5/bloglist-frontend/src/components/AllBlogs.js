637d8aab3715a0a10f2b6073671e35d2d1f05ce0
Jevgenij <sssgustavsss@gmail.com>
2023-06-15 08:31:41 +0300

    5.10

--- part5/bloglist-frontend/src/components/AllBlogs.js
+++ part5/bloglist-frontend/src/components/AllBlogs.js
@@ -10,13 +10,6 @@ const Blogs = ({ setNotification, notification, user, blogs, handleLogout }) =>
     //useRef
     const blogFormRef = useRef()
 
-    // const blogArr = []
-
-    // blogs.map(blog =>
-    // (
-    //     blogArr.push(< Blog setNotification={setNotification} key={blog.id} blog={blog} />)
-    // ))
-
     return (
         <div>
             <h2>blogs</h2>
@@ -29,13 +22,11 @@ const Blogs = ({ setNotification, notification, user, blogs, handleLogout }) =>
                 <NewBlogForm blogFormRef={blogFormRef} setNotification={setNotification} />
             </Togglable>
 
-            {/* {blogArr.sort((a, b) => Number(a.likes) - Number(b.likes))} */}
             {
                 blogs
                     .sort((a, b) => a.likes - b.likes)
                     .map(blog => (< Blog setNotification={setNotification} key={blog.id} blog={blog} />))
             }
-
         </div>
     )
 }
