2e9874e893fa1dfa5a988298897078e167526f79
Jevgenij <sssgustavsss@gmail.com>
2023-06-25 21:25:54 +0300

    6.2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -12,11 +12,11 @@ const counterReducer = (state = initialState, action) =>
     case 'GOOD':
       return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
       return { ...state, bad: state.bad + 1 }
     case 'ZERO':
-      return state
+      return { ...state, good: state.good = 0, ok: state.ok = 0, bad: state.bad = 0 }
     default: return state
   }
 
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
