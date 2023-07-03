fa1e1277d0a169fa983bf38fedcd413b37b3977e
Jevgenij <sssgustavsss@gmail.com>
2023-07-03 22:16:25 +0300

    7.9

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,45 +1,41 @@
-import { useEffect } from 'react'
-import { useField, useResource } from './hooks'
+import { useEffect } from "react";
+import { useField, useResource } from "./hooks";
 
+const App = () => {
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
 
-const App = () =>
-{
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const handleNoteSubmit = (event) => {
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
 
-  const handleNoteSubmit = (event) =>
-  {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
+  const handlePersonSubmit = (event) => {
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
 
-  const handlePersonSubmit = (event) =>
-  {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value })
-  }
-
-  useEffect(() =>
-  {
-    noteService.getAll()
-    personService.getAll()
+  useEffect(() => {
+    noteService.getAll();
+    personService.getAll();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   return (
     <div>
-
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
         <input {...content} />
         <button>create</button>
       </form>
 
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
@@ -47,9 +43,13 @@ const App = () =>
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part7/ultimate-hooks/src/hooks/index.js
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -1,58 +1,45 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState } from "react";
+import axios from "axios";
 
-export const useField = (type) =>
-{
-    const [value, setValue] = useState('')
+export const useField = (type) => {
+  const [value, setValue] = useState("");
 
-    const onChange = (event) =>
-    {
-        setValue(event.target.value)
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+
+  const getAll = async () => {
+    try {
+      const result = await axios.get(baseUrl);
+      return setResources(result.data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return {
-        type,
-        value,
-        onChange
+  const create = async (resource) => {
+    try {
+      const result = await axios.post(baseUrl, resource);
+      setResources([...resources.concat(result.data)]);
+    } catch (err) {
+      console.log(err);
     }
-}
+  };
 
-export const useResource = (baseUrl) =>
-{
-    const [resources, setResources] = useState([])
+  const service = {
+    create,
+    getAll,
+  };
 
-    const getAll = async () =>
-    {
-        try
-        {
-            const result = await axios.get(baseUrl)
-            return setResources(result.data)
-        }
-        catch (err)
-        {
-            console.log(err)
-        }
-    }
-
-    const create = async (resource) =>
-    {
-        try
-        {
-            const result = await axios.post(baseUrl, resource)
-            setResources([...resources.concat(result.data)])
-        }
-        catch (err)
-        {
-            console.log(err)
-        }
-    }
-
-    const service = {
-        create,
-        getAll
-    }
-
-    return [
-        resources, service
-    ]
-}
+  return [resources, service];
+};
